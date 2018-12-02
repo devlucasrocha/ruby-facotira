83b426098d741485a55ac60d3f66f792f84b6637
devlucasrocha <lucasgeorgerocha@gmail.com>
2018-12-02 02:08:32 -0200

    Refactoring the code

--- factorial.rb
+++ factorial.rb
@@ -2,30 +2,26 @@ class Integer
     def fshort
         value = self
         sum = 1
-        dec = value
-       	value.times do |v|
-       		sum *= dec
-       		dec -= 1
-       	end
-       	sum
-   	end
-   	def flong
-   		value = self
+		value.downto(1) do |n|
+			sum *= n
+		end
+		sum
+	end
+	def flong
+		value = self
         sum = 1
-        dec = value
         list = []
-        value.times do |v|
-       		sum *= dec
-       		list << dec.to_s
-       		list << " x " if dec > 1
-       		dec -= 1
-       	end
-       	list << " = "
-       	list << sum.to_s
-       	show = ""
-       	list.each do |value|
-       		show << value
-       	end
-       	show
-   	end
+		value.downto(1) do |n|
+			sum *= n
+			list << n.to_s
+			list << " x " if n > 1
+		end
+		list << " = "
+		list << sum.to_s
+		show = ""
+		list.each do |value|
+			show << value
+		end
+		show	
+	end
 end
